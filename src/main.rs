55ea98c2e3b4f0f8ed56b320c2df10929afbf400
KazumaTakata <neilbuzztakata@gmail.com>
2025-02-16 18:01:48 +0900

    add: log manager追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashMap,
     fs::{self, File},
+    io::Write,
     os::unix::fs::FileExt,
     path::Path,
     rc::Rc,
@@ -8,6 +9,23 @@ use std::{
 
 fn main() {
     println!("Hello, world!");
+
+    let block = BlockId::new("./data/test.txt".to_string(), 0);
+
+    let mut page = Page::new(400);
+    page.set_string(88, "Hello, world! from page");
+
+    let mut file_manager = FileManager::new(Path::new("data"), 400);
+
+    file_manager.write(&block, &mut page);
+
+    let mut page2 = Page::new(400);
+
+    file_manager.read(&block, &mut page2);
+
+    println!("{}", page2.get_string(88));
+
+    // let mut file = file_manager.get_file("./data/test.txt");
 }
 
 struct BlockId {
@@ -136,6 +154,12 @@ impl FileManager {
         result
     }
 
+    fn length(&mut self, file_name: &str) -> usize {
+        let file = self.get_file(file_name);
+        let file_length = file.metadata().unwrap().len() as usize;
+        file_length / self.block_size
+    }
+
     fn read(&mut self, block_id: &BlockId, page: &mut Page) {
         let block_size = self.block_size;
         let file = self.get_file(block_id.get_file_name());
@@ -160,5 +184,76 @@ impl FileManager {
         file.write_at(&byte_array, offset as u64).unwrap();
 
         return BlockId::new(file_name.to_string(), block_number as u64);
+    }
+}
+
+struct LogManager {
+    file_manager: FileManager,
+    current_block_id: BlockId,
+    log_file: String,
+    log_page: Page,
+    latest_lsn: i32,
+    latest_saved_lsn: i32,
+}
+
+impl LogManager {
+    fn new(mut file_manager: FileManager, log_file: String) -> LogManager {
+        let log_size = file_manager.length(&log_file);
+
+        let mut log_page = Page::new(400);
+
+        let block_id;
+
+        if log_size == 0 {
+            block_id = file_manager.append(&log_file);
+        } else {
+            block_id = BlockId::new(log_file.to_string(), log_size as u64 - 1);
+            file_manager.read(&block_id, &mut log_page);
+        }
+
+        let latest_lsn = 0;
+        let latest_saved_lsn = 0;
+
+        LogManager {
+            file_manager,
+            current_block_id: block_id,
+            log_file,
+            log_page,
+            latest_lsn,
+            latest_saved_lsn,
+        }
+    }
+
+    fn flush(&mut self) {
+        self.file_manager
+            .write(&self.current_block_id, &mut self.log_page);
+        self.latest_saved_lsn = self.latest_lsn;
+    }
+
+    fn append_new_block(&mut self) -> BlockId {
+        let block_id = self.file_manager.append(&self.log_file);
+        self.log_page
+            .set_integer(0, self.file_manager.block_size as i32);
+        self.file_manager.write(&block_id, &mut self.log_page);
+        block_id
+    }
+
+    fn append_record(&mut self, record: &[u8]) -> i32 {
+        let record_length = record.len();
+        let mut boundary = self.log_page.get_integer(0);
+
+        let bytes_needed = 4 + record_length;
+
+        if (boundary as usize) < bytes_needed + 4 {
+            self.flush();
+            self.current_block_id = self.append_new_block();
+            boundary = self.log_page.get_integer(0);
+        }
+
+        let offset = (boundary as usize) - (bytes_needed);
+        self.log_page.set_integer(0, offset as i32);
+        self.log_page.set_bytes(offset, record);
+        self.latest_lsn += 1;
+        self.latest_lsn
     }
 }
