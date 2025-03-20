d3c562702e21ce02516675b0f9d7da4bf1f3fd62
KazumaTakata <neilbuzztakata@gmail.com>
2025-03-20 15:02:42 +0900

    add: table schema structを追加

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{
     collections::HashMap,
     fs::{self, File},
     io::Write,
+    iter::Map,
     os::unix::fs::FileExt,
     path::Path,
     rc::Rc,
@@ -729,6 +730,77 @@ impl Transaction {
         let mut buffer = buffer.borrow_mut();
         let page = buffer.content();
         page.get_string(offset)
+    }
+}
+
+#[derive(Clone)]
+struct TableFieldInfo {
+    field_type: TableFieldType,
+    field_length: i32,
+}
+
+#[derive(Clone)]
+enum TableFieldType {
+    INTEGER,
+    VARCHAR,
+}
+
+struct TableSchema {
+    fields: Vec<String>,
+    field_infos: HashMap<String, TableFieldInfo>,
+}
+
+impl TableSchema {
+    fn new() -> TableSchema {
+        TableSchema {
+            fields: Vec::new(),
+            field_infos: HashMap::new(),
+        }
+    }
+
+    fn add_field(&mut self, field_name: String, field_type: TableFieldType, field_length: i32) {
+        self.fields.push(field_name.clone());
+        self.field_infos.insert(
+            field_name,
+            TableFieldInfo {
+                field_type,
+                field_length,
+            },
+        );
+    }
+
+    fn add_integer_field(&mut self, field_name: String) {
+        self.add_field(field_name, TableFieldType::INTEGER, -1);
+    }
+
+    fn add_string_field(&mut self, field_name: String, field_length: i32) {
+        self.add_field(field_name, TableFieldType::VARCHAR, field_length);
+    }
+
+    fn get_field_type(&self, field_name: String) -> Option<TableFieldType> {
+        let field_info = self.field_infos.get(&field_name);
+        // field_infoが存在しない場合はNoneを返す
+
+        if let Some(field_info) = field_info {
+            return Some(field_info.field_type.clone());
+        }
+
+        return None;
+    }
+
+    fn get_field_length(&self, field_name: String) -> Option<i32> {
+        let field_info = self.field_infos.get(&field_name);
+        // field_infoが存在しない場合はNoneを返す
+
+        if let Some(field_info) = field_info {
+            return Some(field_info.field_length);
+        }
+
+        return None;
+    }
+
+    fn has_field(&self, field_name: String) -> bool {
+        self.field_infos.contains_key(&field_name)
     }
 }
 
