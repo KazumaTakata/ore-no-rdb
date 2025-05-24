4fe201ebfd7e8c052aff1c81345b04335a71189b
KazumaTakata <neilbuzztakata@gmail.com>
2025-05-24 22:39:41 +0900

    feat: implement SetIntegerRecord struct and its log record handling; enhance recovery manager functionality

--- src/recovery_manager.rs
+++ src/recovery_manager.rs
@@ -109,7 +109,8 @@ impl SetStringRecord {
     ) -> i32 {
         let transaction_id_offset = Page::get_integer_size();
         let filename_offset = transaction_id_offset + Page::get_integer_size();
-        let block_number_offset = filename_offset + value.len() + Page::get_integer_size();
+        let block_number_offset =
+            filename_offset + block_id.get_file_name().len() + Page::get_integer_size();
         let offset_value_offset = block_number_offset + Page::get_integer_size();
         let value_offset = offset_value_offset + Page::get_integer_size();
         let record_length = value_offset + value.len() + Page::get_integer_size();
@@ -152,5 +153,131 @@ impl LogRecord for SetStringRecord {
         transaction.set_string(buffer_list, self.block_id.clone(), self.offset, &self.value);
 
         transaction.unpin(buffer_list, buffer_manager, self.block_id.clone());
+    }
+}
+
+struct SetIntegerRecord {
+    transaction_id: i32,
+    block_id: BlockId,
+    offset: usize,
+    value: i32,
+}
+
+impl SetIntegerRecord {
+    pub fn new(page: Page) -> Self {
+        let mut offset = Page::get_integer_size();
+        let transaction_id = page.get_integer(offset);
+        offset += Page::get_integer_size();
+        let filename = page.get_string(offset);
+        offset += String::len(&filename) + Page::get_integer_size();
+        let block_number = page.get_integer(offset);
+        let block_id = BlockId::new(filename, block_number as u64);
+
+        offset += Page::get_integer_size();
+        let offset_value = page.get_integer(offset) as usize;
+        offset += Page::get_integer_size();
+        let value = page.get_integer(offset);
+
+        SetIntegerRecord {
+            transaction_id,
+            block_id,
+            offset: offset_value,
+            value,
+        }
+    }
+
+    fn write_to_log(
+        log_manager: &mut log_manager::LogManager,
+        file_manager: &mut file_manager::FileManager,
+        transaction_id: i32,
+        block_id: &BlockId,
+        offset: usize,
+        value: i32,
+    ) -> i32 {
+        let transaction_id_offset = Page::get_integer_size();
+        let filename_offset = transaction_id_offset + Page::get_integer_size();
+        let block_number_offset =
+            filename_offset + block_id.get_file_name().len() + Page::get_integer_size();
+        let offset_value_offset = block_number_offset + Page::get_integer_size();
+        let value_offset = offset_value_offset + Page::get_integer_size();
+        let record_length = value_offset + Page::get_integer_size();
+
+        let mut page = Page::new(record_length);
+        page.set_integer(0, LogRecordType::SETSTRING as i32);
+        page.set_integer(transaction_id_offset, transaction_id);
+        page.set_string(filename_offset, block_id.get_file_name());
+        page.set_integer(block_number_offset, block_id.get_block_number() as i32);
+        page.set_integer(offset_value_offset, offset as i32);
+        page.set_integer(value_offset, value);
+        let lsn = log_manager.append_record(page.get_data(), file_manager);
+
+        return lsn;
+    }
+}
+
+impl LogRecord for SetIntegerRecord {
+    fn operator_code(&self) -> i32 {
+        LogRecordType::SETSTRING as i32
+    }
+
+    fn transaction_id(&self) -> i32 {
+        self.transaction_id
+    }
+
+    fn undo(
+        &self,
+        transaction: &mut Transaction,
+        file_manager: &mut FileManager,
+        buffer_list: &mut BufferList,
+        buffer_manager: &mut BufferManager,
+    ) {
+        transaction.pin(
+            file_manager,
+            buffer_list,
+            buffer_manager,
+            self.block_id.clone(),
+        );
+        transaction.set_integer(buffer_list, self.block_id.clone(), self.offset, self.value);
+
+        transaction.unpin(buffer_list, buffer_manager, self.block_id.clone());
+    }
+}
+
+struct CheckpointRecord {}
+
+impl CheckpointRecord {
+    pub fn new(page: Page) -> Self {
+        let mut offset = Page::get_integer_size();
+        CheckpointRecord {}
+    }
+
+    fn write_to_log(
+        log_manager: &mut log_manager::LogManager,
+        file_manager: &mut file_manager::FileManager,
+    ) -> i32 {
+        let mut page = Page::new(Page::get_integer_size());
+        page.set_integer(0, LogRecordType::SETSTRING as i32);
+        let lsn = log_manager.append_record(page.get_data(), file_manager);
+
+        return lsn;
+    }
+}
+
+impl LogRecord for CheckpointRecord {
+    fn operator_code(&self) -> i32 {
+        LogRecordType::CHECKPOINT as i32
+    }
+
+    fn transaction_id(&self) -> i32 {
+        return -1;
+    }
+
+    fn undo(
+        &self,
+        transaction: &mut Transaction,
+        file_manager: &mut FileManager,
+        buffer_list: &mut BufferList,
+        buffer_manager: &mut BufferManager,
+    ) {
     }
 }
