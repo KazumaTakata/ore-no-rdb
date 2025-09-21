cad759fcf19c0acb193111745334d94bdc8cde5c
KazumaTakata <neilbuzztakata@gmail.com>
2025-09-21 16:52:55 +0900

    refactor: remove commented-out code and clean up main function

--- src/main.rs
+++ src/main.rs
@@ -184,49 +184,6 @@ fn main() -> Result<()> {
     #[cfg(feature = "with-file-history")]
     rl.save_history("history.txt");
     Ok(())
-
-    // let block = BlockId::new("./data/test.txt".to_string(), 0);
-
-    // let mut page = Page::new(400);
-
-    // page.set_string(88, "Hello, world! from page");
-
-    // let mut file_manager = FileManager::new(Path::new("data"), 400);
-
-    // file_manager.write(&block, &mut page);
-
-    // let mut page2 = Page::new(400);
-
-    // file_manager.read(&block, &mut page2);
-
-    // println!("{}", page2.get_string(88));
-
-    // let mut file = file_manager.get_file("./data/test.txt");
-
-    // let mut log_manager = LogManager::new(&mut file_manager, "data/log".to_string());
-
-    // let block_id = BlockId::new("data/test.txt".to_string(), 0);
-
-    // let buffer = buffer_manager.pin(block_id);
-
-    // if let Some(buffer) = buffer {
-    //     let mut buffer_ref = buffer.borrow_mut();
-    //     let page = buffer_ref.content();
-    //     let integer_1 = page.get_integer(80);
-    //     println!("{}", integer_1);
-    //     page.set_integer(80, integer_1 + 1);
-    //     buffer_ref.set_modified(1, 0);
-
-    //     drop(buffer_ref);
-
-    //     buffer_manager.flush_all(1);
-    // }
-
-    // for i in 0..10 {
-    //     let message = format!("Hello, world! from log {}", i);
-    //     let lsn = log_manager.append_record(message.as_bytes());
-    // }
-
     // log_manager.flush();
 }
 
@@ -271,36 +228,4 @@ mod tests {
         // // テスト後にディレクトリを削除
         // std::fs::remove_dir_all(test_dir).unwrap_or_default();
     }
-
-    // // transactionのテスト
-    // #[test]
-    // fn test_transaction() {
-    //     let test_dir = Path::new("data");
-    //     let block_size = 400;
-    //     let mut file_manager = FileManager::new(test_dir, block_size);
-    //     let mut buffer_manager = BufferManager::new(10);
-    //     let mut lock_table = LockTable::new();
-    //     let mut log_manager = LogManager::new(&mut file_manager, "data/log".to_string());
-    //     let mut buffer_list = BufferList::new();
-    //     let mut transaction = Transaction::new(1);
-
-    //     let block_id = BlockId::new("data/test_file.txt".to_string(), 0);
-    //     let offset = 0;
-
-    //     transaction.pin(
-    //         &mut file_manager,
-    //         &mut buffer_list,
-    //         &mut buffer_manager,
-    //         block_id.clone(),
-    //     );
-    //     transaction.set_integer(&mut buffer_list, block_id.clone(), offset, 42);
-    //     transaction.commit(&mut log_manager, &mut file_manager, &mut buffer_manager);
-
-    //     let mut transaction2 = Transaction::new(2, ConcurrencyManager::new());
-
-    //     let value = transaction2.get_integer(&mut buffer_list, block_id.clone(), offset);
-    //     assert_eq!(value, 42);
-
-    //     // std::fs::remove_dir_all(test_dir).unwrap_or_default();
-    // }
 }
